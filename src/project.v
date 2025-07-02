206249a49d9ae984759f4f246157f63b7506f7a7
frankmauer <frankmauer1812@gmail.com>
2025-07-02 15:46:55 +0700

    Update project.v

--- src/project.v
+++ src/project.v
@@ -9,7 +9,7 @@ module tt_um_asiclab_example (
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
-    output wire [7:0] uio_out,  // IOs: Output path
+    output reg [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
     input  wire       ena,      // always 1 when the design is powered, so you can ignore it
     input  wire       clk,      // clock
@@ -18,6 +18,7 @@ module tt_um_asiclab_example (
 
     wire reset = ~rst_n;
     wire _unuse = &{ena,uio_in,1'bo};
+    wire _unused = &{ena, clk, rst_n, 1'b0};
 
     always @(posedge clk or posedge reset) begin
         if(reset) begin
